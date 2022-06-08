5ad9d18a4fd875c46672ddbf25905a0f4ad150a1
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2022-06-08 22:27:51 +0300

    variable scope

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'not our favourite'
+function upperCaseCustomerName(){
+
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+
+    bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+
+    leastFavoriteCustomer = 'Don Julio'; 
+}
